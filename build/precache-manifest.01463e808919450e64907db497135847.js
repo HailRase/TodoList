d6963475bf18a471365ae7b98508908f0f3861af
Yura Klimko <yurii_klimko@mail.ru>
2021-11-17 17:15:05 +0300

    test deploy

---
+++ build/precache-manifest.01463e808919450e64907db497135847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4faf2660121edc3d048c04eb0155a18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69addd9d88d5bbfc8a0",
+    "url": "/static/css/main.8aead0e7.chunk.css"
+  },
+  {
+    "revision": "557f413042906daa8bf5",
+    "url": "/static/js/2.dac5d9f8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dac5d9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69addd9d88d5bbfc8a0",
+    "url": "/static/js/main.dc2bc996.chunk.js"
+  },
+  {
+    "revision": "4dab62173ea9cd01e5e7",
+    "url": "/static/js/runtime-main.6d0d1559.js"
+  }
+]);
