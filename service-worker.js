911e3d67a57d0f184a14a1ebd641bac2b7a7d8ed
Yura Klimko <yurii_klimko@mail.ru>
2021-11-17 16:57:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e872eb14a6423f385ae0d22ad7f4e9c0.js"
+  "/precache-manifest.01463e808919450e64907db497135847.js"
 );
 
 self.addEventListener('message', (event) => {
