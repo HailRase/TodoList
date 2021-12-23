4e13321af015a1120b10b7ca449b4db19f50cd9f
Юра <yurii_klimko@mail.ru>
2021-12-24 02:52:04 +0300

    Updates

---
+++ precache-manifest.2bf1178fee9f41e961ab3a9512133e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32a5b45ea143a961be936c4474ce786",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "815cede7494ce6df33c2",
+    "url": "/TodoList/static/css/main.10b1a031.chunk.css"
+  },
+  {
+    "revision": "3784607d0c211780dd2a",
+    "url": "/TodoList/static/js/2.f61c373c.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TodoList/static/js/2.f61c373c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815cede7494ce6df33c2",
+    "url": "/TodoList/static/js/main.736fc287.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
